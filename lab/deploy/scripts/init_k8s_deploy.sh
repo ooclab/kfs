ed47f2d1e4ed4d1db485dbf2bbad4ebd641baaef
gwind <gwindfree@gmail.com>
2020-04-12 20:22:55 +0800

    忽略错误信息

--- lab/deploy/scripts/init_k8s_deploy.sh
+++ lab/deploy/scripts/init_k8s_deploy.sh
@@ -4,7 +4,7 @@ export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 CLUSTER_NAME="$1"
 
-INSTALL_DIR=${KFS_HOME}/ooclab
+INSTALL_DIR=${KFS_HOME}/kfslab
 K8S_BIN_DIR="${INSTALL_DIR}/k8s/bin"
 PKG_DIR="${INSTALL_DIR}/pkgs"
 
@@ -15,7 +15,7 @@ fi
 
 echo -e "当前目录 $ROOT_DIR\n配置目录 $CLUSTER_NAME"
 
-if [ -n "$(ls -A $INSTALL_DIR)" ]; then
+if [ -n "$(ls -A $INSTALL_DIR > /dev/null 2>&1 )" ]; then
    echo "初始化目录 \"$INSTALL_DIR\" 非空！如果确定继续，请手动删除该目录。"
    exit 1
 fi
